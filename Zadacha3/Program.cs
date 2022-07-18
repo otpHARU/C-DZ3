4d6d0fa96a3d4d37b79d3bdb7551e9cbb37c6c2f
Haru <harumolodec@mail.ru>
2022-07-18 18:42:24 +0700

    Zadacha3 END

---
+++ Zadacha3/Program.cs
@@ -0,0 +1,24 @@
+﻿// Напишите программу, которая принимает на вход число (N) и выдаёт таблицу кубов чисел от 1 до N.
+
+// 3 -> 1, 8, 27
+// 5 -> 1, 8, 27, 64, 125
+
+// Первое решение
+
+// Console.WriteLine("Введите число: ");
+// int number = Convert.ToInt32(Console.ReadLine());
+// int i = 1;
+// while (i <= number)
+// {
+//     Console.WriteLine("Результат: " + (i*i*i));
+//     i++;
+// }
+
+// Второе решение
+
+// Console.WriteLine("Введите число: ");
+// int number = Convert.ToInt32(Console.ReadLine());
+// for (int i = 1; i <= number; i++)
+// {
+//     Console.WriteLine($"Результат: {i*i*i}");
+// }
